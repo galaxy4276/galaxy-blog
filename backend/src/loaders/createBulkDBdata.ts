0a716f1af43259397314aa836fefb8cb093b032b
galaxy4276 <galaxyhi4276@gmail.com>
2021-01-28 07:16:53 +0900

    feat: 가짜데이터 대량 생성 API 01-28 사용자 250명 게시글 250개 태그 30개 ( 각 데이터마다 관계도 id 1번으로 포함 )

---
+++ backend/src/loaders/createBulkDBdata.ts
@@ -0,0 +1,54 @@
+import User from "../models/user";
+import Post from "../models/post";
+import * as faker from 'faker';
+import db from "../models";
+import Tag from '../models/tag';
+
+const createBulkUserData = async () => {
+  for (let i = 0; i <= 250; i++) {
+    await User.create({
+      nickname: faker.name.lastName() + i,
+      email: `galaxy${i + 1}@gmail.com`,
+      password: 'paragraphs',
+    });
+  }
+};
+
+const createBulkPostData = async () => {
+  for (let i = 0; i <= 250; i++) {
+    const [user, post] = await Promise.all(
+      [
+        User.findOne({where: {id: 1}}),
+        Post.create({
+          title: faker.name.title(),
+          body: faker.lorem.sentence(),
+        }),
+      ]);
+
+    await user!.addPost(post);
+  }
+};
+
+const createBulkTagData = async () => {
+  const post = await Post.findOne({ where: { id: 1}});
+  for (let i = 0; i <= 30; i++) {
+    const tag = await Tag.create({
+      name: faker.git.branch(),
+    });
+
+    await tag.addPost(post!);
+  }
+}
+
+async function asyncDB() {
+  try {
+    await db.sequelize.sync({ force: true });
+    await createBulkUserData();
+    await createBulkPostData();
+    await createBulkTagData();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export default asyncDB;
