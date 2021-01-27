29ecededfb45f6f34e204f275d3e5ef78ea82386
galaxy4276 <galaxyhi4276@gmail.com>
2021-01-28 07:24:25 +0900

    feat: 잘못 컴파일된 js 삭제 01-28

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var App_1 = require("./App");
-var config_1 = require("./config");
-var loaders_1 = require("./loaders");
-/* <<< 애플리케이션 환경 값 설정 >>> */
-App_1["default"].set('port', config_1["default"].PORT);
-App_1["default"].set('host', config_1["default"].HOST);
-App_1["default"].listen(App_1["default"].get('port'), loaders_1.whenRunningApp);
