1973e4901920e525dfd4e766b00003408a3b2c79
galaxy4276 <galaxyhi4276@gmail.com>
2021-01-29 13:58:57 +0900

    feat: Promise 기반 문자열 hash화 API 작성 01-29

--- backend/src/loaders/genSaltAndPassword.ts
+++ backend/src/loaders/genSaltAndPassword.ts
@@ -1,28 +1,20 @@
 import * as bcrypt from "bcrypt";
 import AppSetting from "../config";
 
-export default async (password: string) => {
-  if (AppSetting.NODE_ENV === 'development') {
-    const cryptedPW: string = await bcrypt.genSalt(12, (err, salt) => {
-      if (err) throw Error('salt 값 생성 불가');
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) throw Error('비밀번호 hash 실패!');
-        return hash;
+export default (password: string): Promise<string> =>
+  new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err: Error, salt: string) => {
+
+      if (err) throw reject(err);
+
+      if (AppSetting.NODE_ENV === 'development')
+        console.log('12 라운드 기반 salt 값 생성!');
+
+      bcrypt.hash(password, salt, (err: Error, hash: string) => {
+        if (err) reject(err);
+        if (AppSetting.NODE_ENV === 'development')
+          console.log('salt 기반 hash 값 생성!');
+        resolve(hash);
       });
     });
-
-    return cryptedPW;
-  } else {
-    const cryptedPW: string = await bcrypt.genSalt(12, (err, salt) => {
-      if (err) throw Error('salt 값 생성 불가');
-      if (salt) console.log('12 라운드 salt 값 생성!');
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) throw Error('비밀번호 hash 실패!');
-        console.log('salt 기반 hash 값 생성!');
-        return hash;
-      });
-    });
-
-    return cryptedPW;
-  }
-};
+  });
--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -1,7 +1,9 @@
 import whenRunningApp from './run-app';
 import asyncDB from "./createBulkDBdata";
+import genSaltAndPassword from "./genSaltAndPassword";
 
 export {
   whenRunningApp,
   asyncDB,
+  genSaltAndPassword
 };
