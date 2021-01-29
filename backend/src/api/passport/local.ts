ecd87327c0504ebdd9cc7fbda1f9b85b67d05af1
galaxy4276 <galaxyhi4276@gmail.com>
2021-01-29 22:26:55 +0900

    feat: 로컬 로그인 버그 수정 01-29 Error: Missing Credentials

--- backend/src/api/passport/local.ts
+++ backend/src/api/passport/local.ts
@@ -4,27 +4,31 @@ import {PassportStatic} from "passport";
 import * as bcrypt from 'bcrypt';
 
 export default (passport: PassportStatic) => {
-  passport.use(new LocalStrategy(
-    async (nickname, password, done) => {
+  passport.use(new LocalStrategy({
+      usernameField: 'nickname',
+      passwordField: 'password',
+    },
+    async (nickname: string, password: string, done) => {
       const user = await User.findOne({
         where: { nickname }
       });
-
-      console.log('local login user: ', user);
 
       if (!user) {
         return done(null, false, {message: '해당 유저가 존재하지 않습니다.'});
       }
 
-      const verify = bcrypt.compare(password, user.password,
+      await bcrypt.compare(password, user.password,
         (err: Error, result: boolean) => {
-        if (err) throw err;
-        return result;
-      });
-
-      return verify
-        ? done(null, user)
-        : done(null, false, { message: '비밀번호가 일치하지 않습니다.'});
+          if (err) throw err;
+          if (!result) {
+            return done(
+              null,
+              false,
+              { message: '비밀번호가 정확하지 않습니다.' }
+            );
+          }
+          return done(null, user);
+        });
     }
   ));
 };
