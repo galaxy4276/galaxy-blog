84fef3bd3d953b6497be0829f46a7e9158d511c6
galaxy4276 <galaxyhi4276@gmail.com>
2021-02-03 18:01:06 +0900

    feat: CRUD API 간단 작성 02-03

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express';
-import {getPost, post} from "../api/post";
+import {getPost, patchPost, post, deletePost} from "../api/post";
 
 
 const postRouter = Router();
 
 postRouter.post('/', post);
 postRouter.get('/:id', getPost);
+postRouter.patch('/:id', patchPost);
+postRouter.delete('/:id', deletePost);
 
 
 export default postRouter;
